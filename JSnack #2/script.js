679bf120472505c7c633bd35d480a7b8de5ffb5f
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-04 15:25:19 +0200

    Restituzione a video dei console.log

--- JSnack #2/script.js
+++ JSnack #2/script.js
@@ -1,5 +1,5 @@
 // Utente inserisce due stringhe e stampo prima la più grande
-var strUno, strDue, strUnoLengt, strDueLengt;
+var strUno, strDue, strUnoLengt, strDueLengt, risultato;
 
 strUno = prompt('Inserisci la prima parola');
 strDue = prompt('Inserisci la seconda parola');
@@ -7,18 +7,31 @@ strDue = prompt('Inserisci la seconda parola');
 strUnoLengt = strUno.length;
 strDueLengt = strDue.length;
 
+risultato = document.getElementById('risultato');
+
 console.log(strUnoLengt);
 console.log(strDueLengt);
 
 
 
 if (strUnoLengt > strDueLengt) {
+
+  risultato.innerHTML = 'la parola più lunga é: ' + strUno + '.' + '<br>' + 'La stinga ordinata é: ' + strUno + ', ' + strDue ;
+
   console.log('la parola più lunga é: ' + strUno);
   console.log(strUno, strDue);
 
 }else if (strUnoLengt < strDueLengt) {
+
+  risultato.innerHTML = 'la parola più lunga é: ' + strDue + '.' +  '<br>' + 'La stinga ordinata é: ' + strDue + ', ' + strUno ;
+
   console.log('la parola più lunga é: ' + strDue);
   console.log(strDue, strUno);
+
 } else {
+
+  risultato.innerHTML = 'Le parole hanno la stessa lunghezza';
+
   console.log('Le parole hanno la stessa lunghezza');
+
 }
